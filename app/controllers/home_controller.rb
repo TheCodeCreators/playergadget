beb675771ca798cec012206ca5e3723c9353aa18
Raluca Pintilii <ralucapintilii@gmail.com>
2017-09-19 16:36:47 +0100

    Continued implementation of homepage layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@
 class HomeController < ApplicationController
   # home
   def index
-    @top_articles = Article.all.first(4)
+    @top_articles = Article.all.first(2)
     @articles = Article.all
+    @mini_articles = Article.all.last(4)
   end
 end
