425c81c802a78b3942656f67a8e914d80002579a
Raluca Pintilii <ralucapintilii@gmail.com>
2017-09-19 19:04:12 +0100

    Homepage' articles have now their images displayed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,8 @@ class HomeController < ApplicationController
   # home
   def index
     @top_articles = Article.all.first(2)
+    @top_left_article = Article.first
+    @top_right_article = Article.second
     @articles = Article.all
     @mini_articles = Article.all.last(4)
   end
