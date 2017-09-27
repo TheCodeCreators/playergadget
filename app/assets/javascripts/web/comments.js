1b7d28e1f5f37650f7a2d372925994c0f8a2805c
Raluca Pintilii <ralucapintilii@gmail.com>
2017-09-27 18:47:54 +0100

    Added destroy and edit actions for comments

--- app/assets/javascripts/web/comments.js
+++ app/assets/javascripts/web/comments.js
@@ -1,16 +1,20 @@
 document.addEventListener("turbolinks:load", function() {
-  $('.link-to-show-replies').on('click', function(){
-    $(this).addClass('hidden')
-    $(this).next('.replies-row').removeClass('hidden');
-  })
+  // On click should display comments.
+  // $('.link-to-show-replies').on('click', function(){
+  //   $(this).addClass('hidden')
+  //   $(this).next('.replies-row').removeClass('hidden');
+  // })
 
+  // Auto-submits form on pressing the "enter" key.
   $('.new_comment').keypress(function(e){
     if(e.which == 13){
       $(this).closest('form').submit();
     }
   });
-  $('.reply-to-a-comment').on('click', function(){
-    $(this).next('.new-reply-form').toggleClass('hidden');
+
+  // Toggles the comment textarea.
+  $('.reply-action').on('click', function(){
+    $(this).parent().next('.new-reply-form').toggleClass('hidden');
   });
 
 });
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :find_commentable
+  before_action :find_commentable, only: %i[create] 
 
   def new
     @comment = current_user.comments.new
@@ -16,6 +16,32 @@ class CommentsController < ApplicationController
     end
     redirect_back(fallback_location: root_path)
   end
+
+  def edit
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.assign_attributes(comment_params)
+    if (current_user == @comment.user) || current_user.admin?
+      @comment.destroy
+      redirect_back(fallback_location: root_path)
+    else
+      flash.now[:danger] = "error"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if (current_user == @comment.user) || current_user.admin?
+      @comment.destroy
+      flash[:notice] = 'Comment successfully deleted.'
+    else
+      flash.now[:danger] = "error"
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
 
   private
 
