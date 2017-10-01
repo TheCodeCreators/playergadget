9c390b5d2ede3340d839e4af5891da72f3216765
Raluca Pintilii <ralucapintilii@gmail.com>
2017-10-01 18:48:51 +0100

    Changed side articles gathering syntax

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,11 @@ class ArticlesController < ApplicationController
     redirect_to root_path unless @article.published? || current_user.try(:admin?)
     @comments = @article.comments.order(created_at: :asc)
     # Returns all recent published articles minus the main @article.
-    @side_articles = Article.published.recent.reject { |a| a == @article }
+    @side_articles = Article.published
+                            .recent
+                            .where
+                            .not(id: @article.id)
+                            .order(published_at: :desc)
+                            .limit(5)
   end
 end
