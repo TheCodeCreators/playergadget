1e9c5882953c23b31e7bfe5a132ed8233ee67831
Raluca Pintilii <ralucapintilii@gmail.com>
2017-10-02 16:47:19 +0100

    Removed social bootstrap buttons

--- app/assets/javascripts/web/shared.js
+++ app/assets/javascripts/web/shared.js
@@ -1,9 +1,3 @@
 document.addEventListener("turbolinks:load", function() {
   $('[data-toggle="tooltip"]').tooltip();
-  $('.fb-icon').on('click', function(){
-    $(this).siblings('.fb-share-button').trigger('click');
-  });
-  $('.twitter-icon').on('click', function(){
-    $(this).siblings('.twitter-share-button')[0].click();
-  });
 });
