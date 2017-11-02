03e8ede12d1bad1667c8eb254a381f0cf80cd810
Raluca Pintilii <ralucapintilii@gmail.com>
2017-11-02 17:21:36 +0000

    Added margin between article and add and increased the number of articles per page to 7

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   def index
     @articles = Article.published
                        .order(published_at: :desc)
-                       .paginate(page: params[:page], per_page: 2)
+                       .paginate(page: params[:page], per_page: 7)
     @highlights = Highlight.includes(:article).references(:article).active
     respond_to do |format|
       format.html
