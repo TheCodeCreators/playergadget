ccb41169500c2d2b331a7a7535c5dde210829734
Raluca Pintilii <ralucapintilii@gmail.com>
2017-11-02 21:22:13 +0000

    Fixed infinite scroll issue

--- app/assets/javascripts/web/home.js
+++ app/assets/javascripts/web/home.js
@@ -4,6 +4,7 @@ document.addEventListener("turbolinks:load", function() {
     $(window).scroll(function() {
       var url = $('.pagination .next_page').attr('href');
       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 150) {
+        $('.pagination').text("Please Wait...");
         return $.getScript(url);
       }
     });
