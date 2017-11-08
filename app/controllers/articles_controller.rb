80a4b1953d7f942fa7feaef91ef4b1a12252a8ae
Pedro Coutinho <pdcoutinho@gmail.com>
2017-11-08 13:27:47 +0000

    Added tags to articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  # rubocop:disable Metrics/AbcSize
   def show
     @article = Article.friendly.find(params[:id])
     # Only admins can see unpublished articles
     redirect_to root_path unless @article.published? || current_user.try(:admin?)
     @comments = @article.comments.order(created_at: :asc)
+    # tags for the tag bar
+    @tags = Article.published.tags_on(:tags)
+                   .where
+                   .not('name = ?', params[:id])
+                   .order(taggings_count: :desc)
     # Returns all recent published articles minus the main @article.
     @side_articles = Article.published
                             .recent
@@ -14,4 +20,5 @@ class ArticlesController < ApplicationController
                             .order(published_at: :desc)
                             .limit(3)
   end
+  # rubocop:enable Metrics/AbcSize
 end
