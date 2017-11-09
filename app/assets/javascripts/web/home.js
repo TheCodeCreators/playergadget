db86e6e782132bed2ba8d336cd66e804c3e60812
Pedro Coutinho <pdcoutinho@gmail.com>
2017-11-09 16:19:19 +0000

    Infinite loading improvements, closes #59, closes #58

--- app/assets/javascripts/web/home.js
+++ app/assets/javascripts/web/home.js
@@ -4,7 +4,7 @@ document.addEventListener("turbolinks:load", function() {
     if ($('.pagination').length) {
       $(window).scroll(function() {
         var url = $('.pagination .next_page').attr('href');
-        if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 150) {
+        if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 250) {
           $('.pagination').text("Please Wait...");
           return $.getScript(url);
         }
